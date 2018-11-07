bdc04084b8e7d02f4e91d180e5d592323225e18a
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-11-07 13:44:12 -0500

    Added TI Example code

--- ADC_DAC/main.c
+++ ADC_DAC/main.c
@@ -1,12 +1,57 @@
-#include <msp430.h> 
+#include <msp430.h>
 
+#define CALADC12_15V_30C  *((unsigned int *)0x1A1A)   // Temperature Sensor Calibration-30 C
+                                                      //See device datasheet for TLV table memory mapping
+#define CALADC12_15V_85C  *((unsigned int *)0x1A1C)   // Temperature Sensor Calibration-85 C
 
-/**
- * main.c
- */
+unsigned int temp;
+volatile float temperatureDegC;
+volatile float temperatureDegF;
+
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  REFCTL0 &= ~REFMSTR;                      // Reset REFMSTR to hand over control to
+                                            // ADC12_A ref control registers
+  ADC12CTL0 = ADC12SHT0_8 + ADC12REFON + ADC12ON;
+                                            // Internal ref = 1.5V
+  ADC12CTL1 = ADC12SHP;                     // enable sample timer
+  ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense i/p
+  ADC12IE = 0x001;                          // ADC_IFG upon conv result-ADCMEMO
+  __delay_cycles(100);                       // delay to allow Ref to settle
+  ADC12CTL0 |= ADC12ENC;
+
+  while(1)
+  {
+    ADC12CTL0 &= ~ADC12SC;
+    ADC12CTL0 |= ADC12SC;                   // Sampling and conversion start
+
+    __bis_SR_register(LPM4_bits + GIE);     // LPM0 with interrupts enabled
+    __no_operation();
+
+    // Temperature in Celsius. See the Device Descriptor Table section in the
+    // System Resets, Interrupts, and Operating Modes, System Control Module
+    // chapter in the device user's guide for background information on the
+    // used formula.
+    temperatureDegC = (float)(((long)temp - CALADC12_15V_30C) * (85 - 30)) /
+            (CALADC12_15V_85C - CALADC12_15V_30C) + 30.0f;
+
+    // Temperature in Fahrenheit Tf = (9/5)*Tc + 32
+    temperatureDegF = temperatureDegC * 9.0f / 5.0f + 32.0f;
+
+    __no_operation();                       // SET BREAKPOINT HERE
+  }
+}
+
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC12ISR (void)
+{
+  switch(__even_in_range(ADC12IV,34))
+  {
+  case  6:                                  // Vector  6:  ADC12IFG0
+    temp = ADC12MEM0;                       // Move results, IFG is cleared
+    __bic_SR_register_on_exit(LPM4_bits);   // Exit active CPU
+    break;
+  default: break;
+  }
 }
