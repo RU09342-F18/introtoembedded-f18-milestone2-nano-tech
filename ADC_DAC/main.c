861e301d3222816463f38be49d0dd78f81d08cde
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-11-07 15:21:37 -0500

    Added BASIC ADC functionality

--- ADC_DAC/main.c
+++ ADC_DAC/main.c
@@ -1,57 +1,140 @@
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2012, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP430 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP430 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
+ * for an API functional library-approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//   MSP430F552x Demo - ADC12, Sample A0, Set P1.0 if A0 > 0.5*AVcc
+//
+//   Description: A single sample is made on A0 with reference to AVcc.
+//   Software sets ADC12SC to start sample and conversion - ADC12SC
+//   automatically cleared at EOC. ADC12 internal oscillator times sample (16x)
+//   and conversion. In Mainloop MSP430 waits in LPM0 to save power until ADC12
+//   conversion complete, ADC12_ISR will force exit from LPM0 in Mainloop on
+//   reti. If A0 > 0.5*AVcc, P1.0 set, else reset.
+//
+//                MSP430F552x
+//             -----------------
+//         /|\|                 |
+//          | |                 |
+//          --|RST              |
+//            |                 |
+//     Vin -->|P6.0/CB0/A0  P1.0|--> LED
+//
+//   Bhargavi Nisarga
+//   Texas Instruments Inc.
+//   April 2009
+//   Built with CCSv4 and IAR Embedded Workbench Version: 4.21
+//******************************************************************************
+
 #include <msp430.h>
 
-#define CALADC12_15V_30C  *((unsigned int *)0x1A1A)   // Temperature Sensor Calibration-30 C
-                                                      //See device datasheet for TLV table memory mapping
-#define CALADC12_15V_85C  *((unsigned int *)0x1A1C)   // Temperature Sensor Calibration-85 C
-
-unsigned int temp;
+int storage = 0;
+int voltage;
 volatile float temperatureDegC;
 volatile float temperatureDegF;
+unsigned char high;
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  REFCTL0 &= ~REFMSTR;                      // Reset REFMSTR to hand over control to
-                                            // ADC12_A ref control registers
-  ADC12CTL0 = ADC12SHT0_8 + ADC12REFON + ADC12ON;
-                                            // Internal ref = 1.5V
-  ADC12CTL1 = ADC12SHP;                     // enable sample timer
-  ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense i/p
-  ADC12IE = 0x001;                          // ADC_IFG upon conv result-ADCMEMO
-  __delay_cycles(100);                       // delay to allow Ref to settle
+  ADC12CTL0 = ADC12SHT02 + ADC12ON;         // Sampling time, ADC12 on
+  ADC12CTL1 = ADC12SHP;                     // Use sampling timer
+  ADC12IE = 0x01;                           // Enable interrupt
   ADC12CTL0 |= ADC12ENC;
+  P6SEL |= 0x01;                            // P6.0 ADC option select
+  P1DIR |= 0x01;                            // P1.0 output
 
-  while(1)
+
+  UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+  UCA1CTL1 |= UCSSEL_1;                     // CLK = ACLK
+  UCA1BR0 = 0x03;                           // 32kHz/9600=3.41 (see User's Guide)
+  UCA1BR1 = 0x00;                           //
+  UCA1MCTL = UCBRS_3+UCBRF_0;               // Modulation UCBRSx=3, UCBRFx=0
+  UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+
+  UCA1TXBUF = 12;
+  while (1)
   {
-    ADC12CTL0 &= ~ADC12SC;
-    ADC12CTL0 |= ADC12SC;                   // Sampling and conversion start
+    ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
 
-    __bis_SR_register(LPM4_bits + GIE);     // LPM0 with interrupts enabled
-    __no_operation();
-
-    // Temperature in Celsius. See the Device Descriptor Table section in the
-    // System Resets, Interrupts, and Operating Modes, System Control Module
-    // chapter in the device user's guide for background information on the
-    // used formula.
-    temperatureDegC = (float)(((long)temp - CALADC12_15V_30C) * (85 - 30)) /
-            (CALADC12_15V_85C - CALADC12_15V_30C) + 30.0f;
-
-    // Temperature in Fahrenheit Tf = (9/5)*Tc + 32
-    temperatureDegF = temperatureDegC * 9.0f / 5.0f + 32.0f;
-
-    __no_operation();                       // SET BREAKPOINT HERE
+    __bis_SR_register(LPM0_bits + GIE);     // LPM0, ADC12_ISR will force exit
+    __no_operation();                       // For debugger
   }
 }
 
-#pragma vector=ADC12_VECTOR
-__interrupt void ADC12ISR (void)
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
 {
   switch(__even_in_range(ADC12IV,34))
   {
   case  6:                                  // Vector  6:  ADC12IFG0
-    temp = ADC12MEM0;                       // Move results, IFG is cleared
-    __bic_SR_register_on_exit(LPM4_bits);   // Exit active CPU
-    break;
-  default: break;
+     storage = ADC12MEM0;
+     storage = storage * 33 ;
+     storage = storage / 495;
+     voltage = storage * 100;
+     storage = storage * 100;
+
+
+            high = voltage >> 8;
+                  UCA1TXBUF = high;
+                  UCA1TXBUF = voltage;
+
+                  if (ADC12MEM0 >= 0x7ff){                 // ADC12MEM = A0 > 0.5AVcc?
+                  P1OUT |= BIT0;                        // P1.0 = 1
+                  }
+                else{
+                  P1OUT &= ~BIT0;                       // P1.0 = 0
+                }
+
+                __bic_SR_register_on_exit(LPM0_bits);   // Exit active CPU
+default: break;
   }
 }
