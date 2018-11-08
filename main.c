101897075ae36de8cc24e829ebdc88f413619e7c
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-11-07 20:33:26 -0500

    Added Timer_Setup

--- main.c
+++ main.c
@@ -153,6 +153,17 @@ void Board_Setup(){
     P6SEL |= 0x01;                            // P6.0 ADC option select
     P1DIR |= 0x01;                            // P1.0 output
 }
+void Timer_Setup{
+    //P1DIR |= BIT2+BIT3;                       // P1.2 and P1.3 output
+    //P1SEL |= BIT2+BIT3;                       // P1.2 and P1.3 options select
+    TA0CCR0 = 65536;                          // PWM Period
+    TA0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TA0CCTL2 = OUTMOD_7;                      // CCR2 reset/set
+    TA0CTL = TASSEL_2 + MC_1 + TACLR;         // SMCLK, up mode, clear TAR
+
+    TA0CCR1 = 65535;                            // CCR1 PWM
+    TA0CCR2 = 65534;                            // CCR2 PWM
+}
 
 float Convert_VtoR(float vout){
   float R2_value;
