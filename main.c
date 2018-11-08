8c52773997c488679ae7d1cf4d69c48dc5e4bca9
Helen <panh1@students.rowan.edu>
2018-11-07 20:38:36 -0500

    added the function to convert the resistance value to temperature

--- main.c
+++ main.c
@@ -74,8 +74,8 @@ volatile float temperatureDegC;
 volatile float temperatureDegF;
 unsigned char high;
 
-float Convert_VtoR(float vout);
-float Convert_RtoT(float resist);
+float Convert_VtoR(float vout);             // funtion for converting the voltage value to resistance 
+float Convert_RtoT(float resist);           // function for converting the resistance value to temperature
 
 int main(void)
 {
@@ -150,4 +150,31 @@ float Convert_VtoR(float vout){
   R2_value = (vout * 10000) / (vout - 3.3);
 
   return R2_value;
+}
+
+float Convert_RtoT(float resist){
+  float temperature;
+  resist = R2_value;
+
+  if ((resist <= 32554) & (resist > 19872)){
+    temperature = - 0.0008 * resist + 25.311;
+  }
+  else if ((resist <= 19872) & (resist > 10000)){
+    temperature = - 0.0015 * resist + 39.319;
+  }
+  else if ((resist <= 10000) & (resist > 4372)){
+    temperature = - 0.0035 * resist + 59.061;
+  }
+  else if ((resist <= 4372) & (resist > 1753)){
+    temperature = - 0.0094 * resist + 84.646;
+  }
+  else if ((resist <= 1753) & (resist > 786)){
+    temperature = - 0.0256 * resist + 113.46;
+  }
+  else if ((resist <= 786) & (resist > 442.6)){
+    temperature = - 0.0579 * resist + 139.74;
+  }
+  else if ((resist <= 442.6) & (resist > 182.6)){
+    temperature = - 0.1334 * resist + 171.64;
+  }
 }
