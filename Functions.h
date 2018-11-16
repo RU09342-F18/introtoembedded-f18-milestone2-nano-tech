ec113b37866b30ef78eae2ed0a92214a5c3e3a3a
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-11-16 13:42:05 -0500

    Made header file

---
+++ Functions.h
@@ -0,0 +1,19 @@
+/*
+ * Functions.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Author: Tyler
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+
+
+float Convert_VtoR(float vout)
+
+float Convert_RtoT(float R2_value)
+
+
+#endif /* FUNCTIONS_H_ */
+
